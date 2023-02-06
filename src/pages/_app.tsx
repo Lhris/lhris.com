3d8227b9f4947e5f26a55c375fe8765294934021
Chris Lee <christopher@bookreport.io>
2023-02-05 22:25:31 -0800

    Use native Next.js font optimization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,19 @@ import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+import { Fredericka_the_Great } from '@next/font/google'
+
+const fredericka = Fredericka_the_Great({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-fredericka',
+})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${fredericka.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>  )
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        fredericka: ['"Fredericka the Great"',  'Cursive']
-      }
+        fredericka: ['var(--font-fredericka)', ...fontFamily.sans],
+      },
     },
   },
   plugins: [],
