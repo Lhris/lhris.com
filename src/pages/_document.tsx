bc4848e7e9263e19d3ebabc8007419ccdd82424a
Chris Lee <christopher@bookreport.io>
2023-02-05 21:50:31 -0800

    import font in _document.tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <link href="https://fonts.googleapis.com/css2?family=Fredericka+the+Great&display=swap" rel="stylesheet"/>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
